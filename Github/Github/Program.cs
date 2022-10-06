da3c9a699c9cf0403902b890236d8e0e9dbc4b14
Tu Nguyen <tun@qssolutions.nl>
2022-10-06 17:06:39 +0700

    Test 4

--- Github/Github/Program.cs
+++ Github/Github/Program.cs
@@ -10,7 +10,8 @@ namespace Github
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            const string version = "XYZ";
+            const string version = "QDH";
+            const string cert = "b66825cb-2c3d-4d51-98a1-cd244f43dcfb";
 
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
