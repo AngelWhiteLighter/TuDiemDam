391e85e0257e9c5c8a7e197a5fe67b37e691b448
Tu Nguyen <tun@qssolutions.nl>
2022-10-06 17:53:56 +0700

    Test 5

--- Github/Github/Program.cs
+++ Github/Github/Program.cs
@@ -10,8 +10,8 @@ namespace Github
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            const string version = "ABC";
-            const string password = "https://support.microsoft.com/en-ca/help/4469913/troubleshooting-windows-device-enrollment-problems-in-microsoft-intune";
+            const string version = "QDH";
+            const string cert = "b66825cb-2c3d-4d51-98a1-cd244f43dcfb";
 
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
